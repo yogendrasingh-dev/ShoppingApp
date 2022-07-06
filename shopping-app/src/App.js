c8e3cd9ea7ed39c41fcb2aba8827db07d3de3a37
Yogendra Singh <yogisingh4yu@gmail.com>
2022-07-06 22:04:49 +0530

    Worked on UI Part.

--- shopping-app/src/App.js
+++ shopping-app/src/App.js
@@ -1,5 +1,12 @@
+import CartScreen from './screen/CartScreen';
+import HomeScreen from './screen/HomeScreen';
+import LoginScreen from './screen/LoginScreen';
+import ProductListScreen from './screen/ProductListScreen';
+import ProductScreen from './screen/ProductScreen';
+import RegisterScreen from './screen/RegisterScreen';
+
 const App = () => {
-	return <div>Hello world!</div>;
+	return <HomeScreen />;
 };
 
 export default App;
